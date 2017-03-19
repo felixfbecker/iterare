584aa6ed4ac7b22b0dba5e5ed635c38ee9b4d3e0
Felix Becker <felix.b@outlook.com>
2017-03-19 11:56:43 +0100

    Correct wording

--- src/map.test.ts
+++ src/map.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert'
 import { MapIterator } from './map'
 
 describe('MapIterator', () => {
-    it('should emit the results of running each element through the predicate', () => {
+    it('should emit the results of running each element through the iteratee', () => {
         const collection = [1, 2, 3, 4][Symbol.iterator]()
         const mapped = new MapIterator(collection, x => x * 2)
         assert.equal(mapped.next().value, 2)
