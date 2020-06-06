3a70457266024a1fa873a618fe55330951a09e0e
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2020-06-06 18:47:14 +0200

    fix: improve typings for the filter operator (#20)
    Co-authored-by: Felix Becker <felix.b@outlook.com>

--- src/filter.ts
+++ src/filter.ts
@@ -1,12 +1,15 @@
-export class FilterIterator<T> implements Iterator<T> {
-    constructor(private source: Iterator<T>, private predicate: (element: T) => boolean) {}
+export class FilterIterator<T, V extends T = T> implements Iterator<T> {
+    constructor(
+        private source: Iterator<T>,
+        private predicate: ((element: T) => element is V) | ((element: T) => boolean)
+    ) {}
 
-    next(): IteratorResult<T> {
+    next(): IteratorResult<V> {
         let result: IteratorResult<T>
         // Skip elements until predicate returns true
         do {
             result = this.source.next()
         } while (!result.done && !this.predicate(result.value))
-        return result
+        return result as IteratorResult<V>
     }
 }
